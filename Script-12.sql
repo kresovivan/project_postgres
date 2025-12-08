205b82d90ae0d1be3a6739e6c88c39d248814ec1
kresovivan <kresovivan@mail.ru>
2025-12-08 21:06:15 +0300

    eee

--- Script-12.sql
+++
@@ -1,7 +0,0 @@
-SELECT ename, sal, deptno,
-  RANK() OVER (PARTITION BY deptno ORDER BY sal DESC) AS rank,
-  lag(sal) OVER (PARTITION BY deptno ORDER BY sal DESC) AS dept_lag,
-  lead(sal) OVER (PARTITION BY deptno) AS dept_lead,
-  sum(sal) OVER (PARTITION BY deptno) AS dept_avg
-FROM emp
-ORDER BY deptno, sal;
--- Script-15.sql
+++
@@ -1,9 +0,0 @@
-WITH ranked AS (
-    SELECT 
-        department,
-        employee,
-        salary,
-        DENSE_RANK() OVER (PARTITION BY department ORDER BY salary DESC) as rank
-    FROM employees
-)
-SELECT * FROM ranked WHERE rank <= 3;
