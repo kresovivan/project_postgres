bb6e40fd16c413c53f7c4fc71cfdfaab168c130a
Kresov Ivan <kresovivan@mail.ru>
2025-12-21 13:09:31 +0300

    21.12.2025

--- SQL_cookbook.sql
+++ SQL_cookbook.sql
@@ -339,7 +339,9 @@ select ename, sal, comm
 from (
          select ename, sal, comm, case when comm is null then 0 else 1 end as is_null
          from emp) x
-order by is_null desc, comm desc
+order by is_null desc, comm desc;
+
+/**/
 
 
 
